d196838edcb24cce618201ba3252de08b33469bf
uihyeon1229 <101084941+uihyeon1229@users.noreply.github.com>
2022-05-17 04:34:07 +0900

    CI-CD테스트25

--- src/main/java/com/project/sangil_be/CiCD/ProfileController.java
+++ src/main/java/com/project/sangil_be/CiCD/ProfileController.java
@@ -2,9 +2,11 @@ package com.project.sangil_be.CiCD;
 
 import lombok.RequiredArgsConstructor;
 
+
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.Arrays;
 import java.util.List;
